0bbf983f6e47e028063c836a6df5bd9feef63258
snow <seyol17@gmail.com>
2026-02-15 21:15:57 +0900

    Initial Commit

---
+++ ShopManager/ShopManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopManagerApp.swift
+//  ShopManager
+//
+//  Created by seyol on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ShopManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
